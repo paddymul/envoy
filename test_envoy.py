00a09139c460915eca21b8a321e771b5414afdf5
Paddy Mullen <paddy@paddymullen.com>
2012-06-16 11:14:30 -0400

    added a test which demonstrates the function of expand_args

--- test_envoy.py
+++ test_envoy.py
@@ -3,7 +3,7 @@ import envoy
 
 class SimpleTest(unittest.TestCase):
 
-    def test_input(self):
+    def _test_input(self):
         r = envoy.run("sed s/i/I/g", "Hi")
         self.assertEqual(r.std_out.rstrip(), "HI")
         self.assertEqual(r.status_code, 0)
@@ -12,13 +12,21 @@ class SimpleTest(unittest.TestCase):
         r = envoy.run("echo -n 'hi'| tr [:lower:] [:upper:]")
         self.assertEqual(r.std_out, "HI")
         self.assertEqual(r.status_code, 0)
+        r = envoy.run2("echo -n 'hi'| tr [:lower:] [:upper:]")
+        self.assertEqual(r.std_out, "HI")
+        self.assertEqual(r.status_code, 0)
 
-    def test_timeout(self):
+    def test_expand_args(self):
+        self.assertEquals(
+            envoy.expand_args("echo -n 'hi'| tr [:lower:] [:upper:]"),
+            [['echo', '-n', 'hi'], ['tr', '[:lower:]', '[:upper:]']])
+
+    def _test_timeout(self):
         r = envoy.run('yes | head', timeout=1)
         self.assertEqual(r.std_out, 'y\ny\ny\ny\ny\ny\ny\ny\ny\ny\n')
         self.assertEqual(r.status_code, 0)
 
-    def test_quoted_args(self):
+    def _test_quoted_args(self):
         sentinel = 'quoted_args' * 3
         r = envoy.run("python -c 'print \"%s\"'" % sentinel)
         self.assertEqual(r.std_out.rstrip(), sentinel)
